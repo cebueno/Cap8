4f8b33ed56eaae94e8170c18f557a5c484453bd3
Cecilia <131454503+cebueno@users.noreply.github.com>
2023-09-01 18:53:34 -0300

    Ganhos.java

--- src/Ganhos.java
+++ src/Ganhos.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Ganhos {
+package PACKAGE_NAME;
+public class Ganhos extends Movimentacao {
+    private int idGanhos;
+    private String tipoReceita;
+    private double somaGanho;
+
+    public Ganhos (int idMovimentacao, int idGanhos, String tipoReceita, double somaGanho){
+        super(idMovimentacao);
+        this.idGanhos = idGanhos;
+        this.tipoReceita = tipoReceita;
+        this.somaGanho = somaGanho;
+    }
+
+    @Override
+    public void salvarMov(int idMovimentacao) {
+        super.salvarMov(idMovimentacao);
+    }
+
+
+
 }
