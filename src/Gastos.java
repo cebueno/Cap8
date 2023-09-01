e8b7ae2f357401940365cbd59e415005c3c46a05
Cecilia <131454503+cebueno@users.noreply.github.com>
2023-09-01 18:54:04 -0300

    Update Gastos.java

--- src/Gastos.java
+++ src/Gastos.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Gastos {
+package PACKAGE_NAME;
+public class Gastos extends Movimentacao {
+    private int idGasto;
+    private String tipoDespesa;
+    private double somaGasto;
+
+    public Gastos(int idMovimentacao, int idGasto, String tipoDespesa, double somaGasto ){
+        super(idMovimentacao);
+        this.idGasto = idGasto;
+        this.tipoDespesa = tipoDespesa;
+        this.somaGasto = somaGasto;
+    }
+
+    @Override
+    public void salvarMov(int idMovimentacao) {
+        super.salvarMov(idMovimentacao);
+    }
+
 }
