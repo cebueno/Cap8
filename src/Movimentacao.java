7d3ace5376b27272b96a062e28b87798b354b4ee
Cecilia <131454503+cebueno@users.noreply.github.com>
2023-09-01 18:55:28 -0300

    Movimentacao.java

--- src/Movimentacao.java
+++ src/Movimentacao.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Movimentacao {
+package PACKAGE_NAME;
+import java.time.LocalDate;
+
+public class Movimentacao {
+    int idMovimentcao;
+    String nmMovimentacao;
+    String dsMovimentacao;
+    double vlMovimentacao;
+    LocalDate dtMovimentacao;
+    int qtdParcelas;
+
+
+    public Movimentacao(int idMovimentacao){
+        this.idMovimentcao = idMovimentacao;
+    }
+    public void salvarMov(int idMovimentacao){
+
+    }
 }
